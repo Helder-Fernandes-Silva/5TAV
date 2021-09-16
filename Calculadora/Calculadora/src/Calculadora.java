8e16ceba9c33e2c5fbac22798ad6afc4cb77e428
Hélder Fernandes da Silva <89917954+Helder-Fernandes-Silva@users.noreply.github.com>
2021-09-16 13:20:29 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/src/Calculadora.java
@@ -0,0 +1,23 @@
+
+public class Calculadora {
+	
+	public int somar(int a, int b) {
+		
+		return a + b;
+	}
+	
+	public int sub(int a, int b) {
+		
+		return a - b;
+	}
+	
+	public int mult(int a, int b) {
+		
+		return a * b;
+	}
+	
+	public float div( float a, float b) {
+		
+		return a / b;
+	}
+}
