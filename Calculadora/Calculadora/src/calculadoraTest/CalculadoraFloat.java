d72cab9dcd02a652f91d89bb78cdf1b5248bd072
Hélder Fernandes da Silva <89917954+Helder-Fernandes-Silva@users.noreply.github.com>
2021-09-25 23:23:39 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/src/calculadoraTest/CalculadoraFloat.java
@@ -0,0 +1,24 @@
+package calculadoraTest;
+
+public class CalculadoraFloat {
+	
+	public float somar(float a, float b) {
+		
+		return a + b;
+	}
+	
+	public float sub(float a, float b) {
+		
+		return a - b;
+	}
+	
+	public float mult(float a, float b) {
+		
+		return a * b;
+	}
+	
+	public float div(float a, float b) {
+		
+		return a / b;
+	}
+}
