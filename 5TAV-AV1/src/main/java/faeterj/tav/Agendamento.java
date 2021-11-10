a393b574d460fc06f5414b5ea2cd1fab887ca3c3
Hélder Fernandes da Silva <89917954+Helder-Fernandes-Silva@users.noreply.github.com>
2021-11-09 21:30:45 -0300

    AV1 de 5TAV

---
+++ 5TAV-AV1/src/main/java/faeterj/tav/Agendamento.java
@@ -0,0 +1,59 @@
+package faeterj.tav;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Agendamento {
+	public int idAgendamento;
+	public int idFuncionario;
+	public String nomeMedico;
+	public String endereco;
+	public LocalDate dataAgendamento;
+	public LocalTime hora;
+	
+	public int getIdAgendamento() {
+		return idAgendamento;
+	}
+	public void setIdAgendamento(int idAgendamento) {
+		this.idAgendamento = idAgendamento;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public LocalTime getHora() {
+		return hora;
+	}
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+	public LocalDate getDataAgendamento() {
+		return dataAgendamento;
+	}
+	public void setDataAgendamento(LocalDate dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
+	}
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(int idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	
+	// Método para verificar se a consulta foi marcada
+	public String Consulta (int idAgendamento, String nomeMedico, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) {
+		return "Consulta marcada com sucesso!"; // confirmação da marcação da consulta
+	}
+	
+	// Método para verificar se o(s) exame(s) foram marcados
+	public String Exame (int idAgendamento, String nomeMedico, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) {
+		return "Exame marcado com sucesso!"; // confirmação da marcação do(s) exame(s)
+	}
+	
+	// Método para verificar se a internação foi realizada
+	public String Internacao (int idAgendamento, String nomeMedico, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) {
+		return "Internação Realizada!"; // confirmação  da internação
+	}
+}
---
+++ 5TAV-AV1/src/test/java/faeterj/tav/teste/AgendamentoTeste.java
@@ -0,0 +1,132 @@
+package faeterj.tav.teste;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+import faeterj.tav.Agendamento;
+import faeterj.tav.Medico;
+import faeterj.tav.Funcionario;
+
+public class AgendamentoTeste {
+
+	//Testando a marcação de consulta com o Oncologista
+	@Test
+	void MarcarConsultaOncologista(){
+		//Arrange
+		Agendamento agendar = new Agendamento();
+		String esperado = "Consulta marcada com sucesso!";
+		
+		LocalTime hora = LocalTime.of(10,0);
+		LocalDate data = LocalDate.of(2021, 12, 15);
+		
+        Medico<String, String> doutor = Medico.Oncologista();
+
+		//Act
+		String resultado = agendar.Consulta(1, doutor.getKey(), doutor.getValue(), 1, hora, data);
+		
+		//Assert
+		assertEquals(esperado, resultado);
+	}
+	
+	//Testando a marcação de consulta com o Ginecologista
+	@Test
+	void MarcarConsultaGinecologista(){
+		//Arrange
+		Agendamento agendar = new Agendamento();
+		String esperado = "Consulta marcada com sucesso!";
+		
+		LocalTime hora = LocalTime.of(15,0);
+		LocalDate data = LocalDate.of(2022, 01, 10);
+		
+        Medico<String, String> doutor = Medico.Ginecologista();
+
+		//Act
+		String resultado = agendar.Consulta(2, doutor.getKey(), doutor.getValue(), 2, hora, data);
+		
+		//Assert
+		assertEquals(esperado, resultado);
+	}
+	
+	//Testando a marcação de consulta com o Cardiologista
+		@Test
+		void MarcarConsultaCardiologista(){
+			//Arrange
+			Agendamento agendar = new Agendamento();
+			String esperado = "Consulta marcada com sucesso!";
+			
+			LocalTime hora = LocalTime.of(11,0);
+			LocalDate data = LocalDate.of(2022, 02, 30);
+			
+	        Medico<String, String> doutor = Medico.Cardiologista();
+
+			//Act
+			String resultado = agendar.Consulta(3, doutor.getKey(), doutor.getValue(), 3, hora, data);
+			
+			//Assert
+			assertEquals(esperado, resultado);
+		}
+		
+		//Testando a marcação de exame
+		@Test
+		void MarcarExame() {
+			//Arrange
+			Agendamento agendar = new Agendamento();
+			String esperado = "Exame marcado com sucesso!";
+			
+			LocalTime hora = LocalTime.of(8,0);
+			LocalDate data = LocalDate.of(2021, 12, 5);
+			
+	        Medico<String, String> doutor = Medico.Laboratorio();
+
+			//Act
+			String resultado = agendar.Exame(1, doutor.getKey(), doutor.getValue(), 1, hora, data);
+			
+			//Assert
+			assertEquals(esperado, resultado);
+		}
+		
+		//Testando a marcação de internação
+		@Test
+		void MarcarInternacao() {
+			//Arrange
+			Agendamento agendar = new Agendamento();
+			String esperado = "Internação realizada!";
+					
+			LocalTime hora = LocalTime.of(7,0);
+			LocalDate data = LocalDate.of(2021, 11, 16);
+					
+	        Medico<String, String> doutor = Medico.Internacao();
+
+					
+			//Act
+			String resultado = agendar.Internacao(1, doutor.getKey(), doutor.getValue(), 1, hora, data);
+					
+			//Assert
+			assertEquals(esperado, resultado);
+		}
+		
+		// Testando se funcionário tem acesso ao plano de saúde
+		@Test
+		void VerificarPlano() {
+			//Arrange
+			Funcionario Funcionario = new Funcionario();
+			
+			int id = 3;
+			String nome = "José";
+			String temPlano = "Sim";
+			
+			// Dados gerais do funcionário
+			String esperado = "ID:" + id + "\n" + "Nome:" + nome + "\n" + "Possui plano: " + temPlano;
+									
+			//Act
+			String resultado = Funcionario.carteirinhaFuncionario(id, nome, true);
+			
+			//Assert
+			assertEquals(esperado,resultado);
+		}
+
+}
