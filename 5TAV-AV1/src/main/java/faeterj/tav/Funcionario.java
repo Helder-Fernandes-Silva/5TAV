46414335d9a65f2ed53cdd523297d31cc4240f6d
Hélder Fernandes da Silva <89917954+Helder-Fernandes-Silva@users.noreply.github.com>
2021-11-09 23:07:26 -0300

    AV1 de 5TAV

---
+++ 5TAV-AV1/src/main/java/faeterj/tav/Funcionario.java
@@ -0,0 +1,39 @@
+package faeterj.tav;
+
+public class Funcionario {
+	public int idFuncionario;
+	public String nomeFuncionario;
+	public boolean possuiPlano;
+	
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(int idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+	public boolean isPossuiPlano() {
+		return possuiPlano;
+	}
+	public void setPossuiPlano(boolean possuiPlano) {
+		this.possuiPlano = possuiPlano;
+	}
+	
+	public String carteirinhaFuncionario (int idFuncionario, String nomeFuncionario, boolean possuiPlano) {
+		int id = idFuncionario;
+		String nome = nomeFuncionario;
+		String temPlano;
+		
+		if (possuiPlano == true) { // verificando se o funcionário se encontra como beneficiário do plano de saúde
+			temPlano = "Sim";
+		} else {
+			temPlano = "Não";
+		}
+		return "ID:" + id + "\n" + "Nome:" + nome + "\n" + "Possui plano: " + temPlano;
+	}	
+}
